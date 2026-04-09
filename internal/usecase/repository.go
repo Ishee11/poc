24b4274a4dbbb3476ac32eb0d5995d49143d9447
Stepan Semenov <ishee@yandex.ru>
2026-04-09 20:14:48 +1000

    refactor: split interfaces and migrate BuyInUseCase to ISP (partial)

--- internal/usecase/repository.go
+++ internal/usecase/repository.go
@@ -19,3 +19,32 @@ type SessionRepository interface {
 	FindByID(tx Tx, sessionID entity.SessionID) (*entity.Session, error)
 	Save(tx Tx, session *entity.Session) error
 }
+
+type OperationWriter interface {
+	Save(tx Tx, op *entity.Operation) error
+}
+
+type OperationReader interface {
+	GetByID(tx Tx, id entity.OperationID) (*entity.Operation, error)
+	GetByRequestID(tx Tx, requestID string) (*entity.Operation, error)
+}
+
+type OperationAggregateReader interface {
+	GetSessionAggregates(tx Tx, sessionID entity.SessionID) (SessionAggregates, error)
+}
+
+type OperationPlayerStateReader interface {
+	GetLastOperationType(tx Tx, sessionID entity.SessionID, playerID entity.PlayerID) (entity.OperationType, bool, error)
+}
+
+type OperationReversalChecker interface {
+	ExistsReversal(tx Tx, targetID entity.OperationID) (bool, error)
+}
+
+type SessionReader interface {
+	FindByID(tx Tx, sessionID entity.SessionID) (*entity.Session, error)
+}
+
+type SessionWriter interface {
+	Save(tx Tx, session *entity.Session) error
+}
