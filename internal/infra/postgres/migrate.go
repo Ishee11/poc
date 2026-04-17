e83ce4b63091c7f1e9c2b56e454cd5c6e0d09e2c
Stepan Semenov <ishee@yandex.ru>
2026-04-17 12:26:01 +1000

    migrations

--- internal/infra/postgres/migrate.go
+++ internal/infra/postgres/migrate.go
@@ -4,23 +4,28 @@ import (
 	"context"
 	"fmt"
 	"io/fs"
+	"sort"
 	"strings"
 
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 func RunMigrations(ctx context.Context, pool *pgxpool.Pool, migrationsFS fs.FS) error {
-	entries, err := fs.ReadDir(migrationsFS, ".")
+	entries, err := fs.ReadDir(migrationsFS, "migrations")
 	if err != nil {
 		return err
 	}
+
+	sort.Slice(entries, func(i, j int) bool {
+		return entries[i].Name() < entries[j].Name()
+	})
 
 	for _, e := range entries {
 		if !strings.HasSuffix(e.Name(), ".up.sql") {
 			continue
 		}
 
-		query, err := fs.ReadFile(migrationsFS, e.Name())
+		query, err := fs.ReadFile(migrationsFS, "migrations/"+e.Name())
 		if err != nil {
 			return err
 		}
